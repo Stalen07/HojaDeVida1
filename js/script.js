aa4b85532669baf8bcc58e23066832988830f592
Stalen07 <alexmf0619@gmail.com>
2024-11-23 21:26:42 -0500

    Actualizancion css

--- js/script.js
+++ js/script.js
@@ -4,14 +4,14 @@ const menuItems = document.querySelectorAll('nav ul li');
 
 // Evento para abrir/cerrar el menú al hacer clic en el icono
 menuButton.addEventListener('click', () => {
-    menu.classList.toggle('show'); // Alterna la clase para mostrar/ocultar el menú
-    menuButton.classList.toggle('active'); // Alterna el estado del botón (hamburguesa a "X")
+    menu.classList.toggle('show'); 
+    menuButton.classList.toggle('active');
 });
 
 // Cerrar el menú cuando se haga clic en una opción
 menuItems.forEach(item => {
     item.addEventListener('click', () => {
-        menu.classList.remove('show'); // Elimina la clase para ocultar el menú
-        menuButton.classList.remove('active'); // Cambia el botón a su estado original
+        menu.classList.remove('show'); 
+        menuButton.classList.remove('active'); 
     });
 });
